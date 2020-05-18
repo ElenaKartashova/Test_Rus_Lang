b1a45afb58fcfbd72b9b84aae118a5ed0a95a078
e.v.kartashova <e.v.kartashova@tinkoff.ru>
2020-05-18 09:56:51 +0300

    правки в xml

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void showContentFiveClass(View view) {
+    }
+
+    public void showContentSixClass(View view) {
+    }
+
+    public void showContentSevenClass(View view) {
+    }
+
+    public void showContentEightClass(View view) {
+    }
+
+    public void showContentNineClass(View view) {
+        Intent intent = new Intent(this, NineClassContentActivity.class);
+        startActivity(intent);
+    }
+
 }
